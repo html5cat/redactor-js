703a9f239c3b2f90c5736b1110d228dca707e3d0
sabareeshk <sabareeshk1991@gmail.com>
2017-07-25 18:21:47 +0530

    image slide template integration

--- redactor/redactor.js
+++ redactor/redactor.js
@@ -96,8 +96,9 @@ var RLANG = {
 		return this.each(function()
 		{
 			var $obj = $(this);
-
+            console.log('obj', $obj)
 			var data = $obj.data('redactor');
+			console.log('data', data);
 			if (!data)
 			{
 				$obj.data('redactor', (data = new Redactor(this, option)));
@@ -899,6 +900,7 @@ var RLANG = {
 				// new line p
 				if (key === 13 && !e.shiftKey && !e.ctrlKey && !e.metaKey)
 				{
+					console.log('key up');
 					if (this.browser('webkit'))
 					{
 						this.formatNewLine(e);
@@ -989,12 +991,12 @@ var RLANG = {
 				{
 					this.opts.buffer = false;
 				}
-
+                
 				// enter
 				if (pre === true && key === 13)
 				{
 					e.preventDefault();
-
+                   console.log('helloo');
 					var html = $(current).parent().text();
 					this.insertNodeAtCaret(this.document.createTextNode('\r\n'));
 					if (html.search(/\s$/) == -1)
@@ -1005,7 +1007,11 @@ var RLANG = {
 
 					return false;
 				}
-
+				//setting enter key to chnage p to br
+                // if (key == 13){
+				// 	e.preventDefault();
+                // //    document.execCommand('insertHTML', false, '<br>');
+				// }
 				// tab
 				if (this.opts.shortcuts && !e.shiftKey && key === 9)
 				{
@@ -3536,6 +3542,7 @@ var RLANG = {
 				//add image inside afigure with close tag
 				var generatedFigure = this.createFigure();
 				generatedFigure.appendChild($(data)[0])
+				// generatedFigure.appendChild(this.createSlideHtml());
 				console.log(data)
 				this._imageSet(generatedFigure.outerHTML, true);
 			}
@@ -3578,7 +3585,86 @@ var RLANG = {
 			//for click event
 			this.observeVideos()
 		},
+		// image slide html
+		createSlideHtml: function()
+		{
+			  var slideimages = '<ul class="slides">' +
+				'<input type="radio" name="radio-btn" id="img-1" checked />'+
+    '<li class="slide-container">'+
+		'<div class="slide">' +
+			'<img src="http://farm9.staticflickr.com/8072/8346734966_f9cd7d0941_z.jpg" />' +
+        '</div>'+
+		'<div class="nav">' +
+			'<label for="img-6" class="prev">&#x2039;</label>' +
+			'<label for="img-2" class="next">&#x203a;</label>' +
+		'</div>'+
+    '</li>'+
+    '<input type="radio" name="radio-btn" id="img-2" />'+
+    '<li class="slide-container">'+
+        '<div class="slide">'+
+          '<img src="http://farm9.staticflickr.com/8504/8365873811_d32571df3d_z.jpg" />'+
+        '</div>'+
+		'<div class="nav">'+
+			'<label for="img-1" class="prev">&#x2039;</label>'+
+			'<label for="img-3" class="next">&#x203a;</label>'+
+		'</div>'+
+    '</li>'+
 
+    '<input type="radio" name="radio-btn" id="img-3" />'+
+    '<li class="slide-container">'+
+        '<div class="slide">'+
+          '<img src="http://farm9.staticflickr.com/8068/8250438572_d1a5917072_z.jpg" />'+
+        '</div>'+
+		'<div class="nav">'+
+			'<label for="img-2" class="prev">&#x2039;</label>'+
+			'<label for="img-4" class="next">&#x203a;</label>'+
+		'</div>'+
+    '</li>'+
+
+    '<input type="radio" name="radio-btn" id="img-4" />'+
+    '<li class="slide-container">'+
+        '<div class="slide">'+
+          '<img src="http://farm9.staticflickr.com/8061/8237246833_54d8fa37f0_z.jpg" />'+
+        '</div>'+
+		'<div class="nav">'+
+			'<label for="img-3" class="prev">&#x2039;</label>'+
+			'<label for="img-5" class="next">&#x203a;</label>'+
+		'</div>'+
+   ' </li>'+
+
+   ' <input type="radio" name="radio-btn" id="img-5" />'+
+    '<li class="slide-container">'+
+        '<div class="slide">'+
+          '<img src="http://farm9.staticflickr.com/8055/8098750623_66292a35c0_z.jpg" />'+
+        '</div>'+
+		'<div class="nav">'+
+			'<label for="img-4" class="prev">&#x2039;</label>'+
+			'<label for="img-6" class="next">&#x203a;</label>'+
+		'</div>'+
+    '</li>'+
+
+    '<input type="radio" name="radio-btn" id="img-6" />'+
+    '<li class="slide-container">'+
+        '<div class="slide">'+
+         ' <img src="http://farm9.staticflickr.com/8195/8098750703_797e102da2_z.jpg" />'+
+        '</div>'+
+		'<div class="nav">'+
+			'<label for="img-5" class="prev">&#x2039;</label>'+
+			'<label for="img-1" class="next">&#x203a;</label>'+
+		'</div>'+
+    '</li>'+
+
+    '<li class="nav-dots">'+
+      '<label for="img-1" class="nav-dot" id="img-dot-1"></label>'+
+      '<label for="img-2" class="nav-dot" id="img-dot-2"></label>'+
+      '<label for="img-3" class="nav-dot" id="img-dot-3"></label>'+
+      '<label for="img-4" class="nav-dot" id="img-dot-4"></label>'+
+      '<label for="img-5" class="nav-dot" id="img-dot-5"></label>'+
+      '<label for="img-6" class="nav-dot" id="img-dot-6"></label>'+
+   ' </li>'+
+'</ul>';
+       return $(slideimages)[0].outerHTML
+		},
 		// INSERT LINK
 		showLink: function()
 		{
