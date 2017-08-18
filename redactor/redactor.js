708f464ea4e44ef4e4e49b6905a6aa894f6a3c92
sabareeshk <sabareeshk1991@gmail.com>
2017-08-18 18:48:48 +0530

    black button highlight added

--- redactor/redactor.js
+++ redactor/redactor.js
@@ -210,12 +210,12 @@ var RLANG = {
 			},
 
 			colors: [
-				'#ffffff', '#000000', '#eeece1', '#1f497d', '#4f81bd', '#c0504d', '#9bbb59', '#8064a2', '#4bacc6', '#f79646', '#ffff00',
+				'#ffffff', '#0c0c0c', '#eeece1', '#1f497d', '#4f81bd', '#c0504d', '#9bbb59', '#8064a2', '#4bacc6', '#f79646', '#ffff00',
 				'#f2f2f2', '#7f7f7f', '#ddd9c3', '#c6d9f0', '#dbe5f1', '#f2dcdb', '#ebf1dd', '#e5e0ec', '#dbeef3', '#fdeada', '#fff2ca',
 				'#d8d8d8', '#595959', '#c4bd97', '#8db3e2', '#b8cce4', '#e5b9b7', '#d7e3bc', '#ccc1d9', '#b7dde8', '#fbd5b5', '#ffe694',
 				'#bfbfbf', '#3f3f3f', '#938953', '#548dd4', '#95b3d7', '#d99694', '#c3d69b', '#b2a2c7', '#b7dde8', '#fac08f', '#f2c314',
 				'#a5a5a5', '#262626', '#494429', '#17365d', '#366092', '#953734', '#76923c', '#5f497a', '#92cddc', '#e36c09', '#c09100',
-				'#7f7f7f', '#0c0c0c', '#1d1b10', '#0f243e', '#244061', '#632423', '#4f6128', '#3f3151', '#31859b', '#974806', '#7f6000'],
+				'#7f7f7f', '#2d2828', '#1d1b10', '#0f243e', '#244061', '#632423', '#4f6128', '#3f3151', '#31859b', '#974806', '#7f6000'],
             fontFamily: [
 				{ font: 'Times New Roman', family: '"Times New Roman", Times, serif'},
 				{ font: 'Courier New', family: '"Courier New", Courier, monospace'},
@@ -1509,7 +1509,7 @@ var RLANG = {
 						var caption = $('#redactor_image_caption').val();
 						$(parent).find($('figcaption')).text(caption);
 						this.modalClose();
-						
+
 					}, this));
 
 				}, this));
@@ -2688,7 +2688,8 @@ var RLANG = {
 				elemColor = $(this).css('background-color')
 				if (key == 'fontcolor')
 					{
-						applyedColor = colors.color
+						// TODO: remove this default value in future because nobody shows the default color.
+						applyedColor = colors.color || "rgb(0, 0, 0)";
 
 					}
 				else if (key == 'backcolor')
@@ -2706,7 +2707,7 @@ var RLANG = {
 		getApplyedColor: function(colors)
 		{
 			var parent = this.getCurrentNode();
-            console.log(this.getSelectedNode(), this.getCurrentNode(), this.getParentNode(), this.getSelectedHtml());
+            // console.log(this.getSelectedNode(), this.getCurrentNode(), this.getParentNode(), this.getSelectedHtml());
 			//selecting the node for formatting
 			if(this.getSelectedNode().nodeType == 3)
 			{
@@ -2753,6 +2754,7 @@ var RLANG = {
 		},
 		getRgb: function(element, colors) 
 		{
+			// console.log('sudo', $(element).css('color'))
 			if ($(element).css('color') !== "rgb(0, 0, 0)")
 			{
 				colors.color = $(element).css('color')
